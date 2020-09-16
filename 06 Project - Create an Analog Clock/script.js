ff7dfb158267a001c58a4407cca8616d906ec05c
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-17 00:45:41 +1000

    the current date is created and the corresponding hour, minute and second values are extracted properly from the date object.

--- 06 Project - Create an Analog Clock/script.js
+++ 06 Project - Create an Analog Clock/script.js
@@ -7,6 +7,13 @@ let hrPosition = 20;
 let minPosition = 180;
 let secPosition = 270;
 
+//the current time will be created 
+//then the hour, minute and second of the current time will be extracted
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr +" Minute: " + min + " Seconds:" + sec);
 
 //move the hands, by transform and rotate properties and the calcualted degrees of the rotations
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
