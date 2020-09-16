94094874c2fe5a0c89958209787f34c363e180a7
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-16 23:39:32 +1000

    finished. Using .createELement, .CreateTextNode and .appendChild methods to extract the alt attribute from an image and changing it to a figcaption and then putting this new caption into the figure element by appendCHild method.

--- 05-06 Add DOM Elements/JS/script.js
+++ 05-06 Add DOM Elements/JS/script.js
@@ -1,1 +1,19 @@
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+console.log(FEATURED);
+console.log(THEIMAGE);
 
+var altText = THEIMAGE.getAttribute("alt");
+console.log(altText);
+
+var captionElement = document.createElement("figcaption");
+console.log(captionElement);
+
+var captionText = document.createTextNode(altText);
+console.log(captionText);
+
+captionElement.appendChild(captionText);
+console.log(captionElement);
+
+FEATURED.appendChild(captionElement);
+THEIMAGE.setAttribute("alt" , "");
