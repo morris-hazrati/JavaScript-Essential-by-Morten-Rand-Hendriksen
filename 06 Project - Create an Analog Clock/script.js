4503153fd3c0432151333d1d02033fdd25b84ce1
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-17 00:37:39 +1000

    the hands rotate by using hand.style.transform = rotate(180deg) code. Next, the positions of the hands should be derived in degrees.

--- 06 Project - Create an Analog Clock/script.js
+++ 06 Project - Create an Analog Clock/script.js
@@ -1,3 +1,15 @@
+//select the hourhand, secondhand and minute hand to process in the rest of the code
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let minPosition = 180;
+let secPosition = 270;
+
+
+//move the hands, by transform and rotate properties and the calcualted degrees of the rotations
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
