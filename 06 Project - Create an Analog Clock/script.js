6eba52d542dc8a0fd3de95bdece5f5bafaec278a
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-17 01:01:58 +1000

    the precise positions of the hands are calculated in degrees from the time.

--- 06 Project - Create an Analog Clock/script.js
+++ 06 Project - Create an Analog Clock/script.js
@@ -3,10 +3,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPosition = 20;
-let minPosition = 180;
-let secPosition = 270;
-
 //the current time will be created 
 //then the hour, minute and second of the current time will be extracted
 var date = new Date();
@@ -14,6 +10,12 @@ let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr +" Minute: " + min + " Seconds:" + sec);
+
+//now, I should transform the time in terms of hour and minute and seconds
+//into degrees which can be shown on the clock face.
+let hrPosition = hr * 360/12 + min * 360/60/12;
+let minPosition = min * 360/60 + sec * 360/60 /60;
+let secPosition = sec * 360/60;
 
 //move the hands, by transform and rotate properties and the calcualted degrees of the rotations
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
