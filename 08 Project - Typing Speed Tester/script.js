6bb88625c621dfbaf2297e82748e58865a05155f
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-18 15:53:45 +1000

    the timer is created and working properly. A helper functioin was created to solve the leadingZero problem to show the time. Next, the spell checking will be created

--- 08 Project - Typing Speed Tester/script.js
+++ 08 Project - Typing Speed Tester/script.js
@@ -17,12 +17,14 @@ console.log("resetButton: " , resetButton);
 const theTimer = document.querySelector(".timer");
 console.log("theTimer: " , theTimer);
 
+var timer = [0,0,0,0];
+
 // 3- create the handler functions
 function start () {
   console.log("start function triggered!");
   let textEnteredLength = testArea.value.length;
   if (textEnteredLength === 0) {
-    setInterval(runTimer , 1000);
+    setInterval(runTimer , 10);
   }
   console.log("textEnteredLength: " , textEnteredLength);
 };
@@ -39,7 +41,22 @@ function reset () {
 
 // 4- create runTimer function to run the runTimer
 function runTimer () {
-  console.log("runTimer function triggered!");
+  // console.log("runTimer function triggered!");
+  timer[0] = Math.floor(timer[3]/100/60);
+  timer[1] = Math.floor(timer[3]/100 - timer[0]*60);
+  timer[2] = Math.floor(timer[3] - timer[1]*100 - timer[0]*6000);
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer [1]) + ":" + leadingZero(timer[2]);
+  timer[3]++;
+  // console.log(currentTime);
+  theTimer.innerHTML = currentTime;
+};
+
+// 5- to help the runTimer, a helping function for leading zero created
+function leadingZero (time) {
+  if (time <= 9) {
+    time = "0" + time;
+  }
+  return time;
 };
 
 // 2- add event listeners to the interactive elements
