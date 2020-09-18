f8071b08d698acc6ddeb2a59a0d2e54208a303ae
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-18 15:23:03 +1000

    event handlers added to the interactive elements and handler functions defined. Next, the timer will be built

--- 08 Project - Typing Speed Tester/script.js
+++ 08 Project - Typing Speed Tester/script.js
@@ -1,1 +1,49 @@
-console.log("The JavaScript is working!");
+console.log("Hi, the JavaScript is working!");
+
+// 1- declare the constants which are the interactive elements of the page
+
+const testWrapper = document.querySelector(".test-wrapper");
+console.log("testWrapper: " , testWrapper);
+
+const testArea = document.querySelector("#test-area");
+console.log("testArea: " , testArea);
+
+const originText = document.querySelector("#origin-text p").innerHTML;
+console.log("originText: " , originText);
+
+const resetButton = document.querySelector("#reset");
+console.log("resetButton: " , resetButton);
+
+const theTimer = document.querySelector(".timer");
+console.log("theTimer: " , theTimer);
+
+// 3- create the handler functions
+function start () {
+  console.log("start function triggered!");
+  let textEnteredLength = testArea.value.length;
+  if (textEnteredLength === 0) {
+    setInterval(runTimer , 1000);
+  }
+  console.log("textEnteredLength: " , textEnteredLength);
+};
+
+function spellCheck () {
+  console.log("spellCheck function triggered!");
+  let textEntered = testArea.value;
+  console.log("textEntered: " , textEntered);
+};
+
+function reset () {
+  console.log("reset function triggered");
+};
+
+// 4- create runTimer function to run the runTimer
+function runTimer () {
+  console.log("runTimer function triggered!");
+};
+
+// 2- add event listeners to the interactive elements
+
+testArea.addEventListener("keypress" , start , false);
+testArea.addEventListener("keyup" , spellCheck , false);
+resetButton.addEventListener("click" , reset , false);
