32df81fed6c28b4108784466831a94b4f6ff81a4
Morris Hazrati pc <morteza.hazrati@gmail.com>
2020-09-20 17:56:24 +1000

    image srcset attribute collection created for each of the images by a helper function makeSrcset.

--- 09 Project - Responsive Images by Using JavaScript/JS/script.js
+++ 09 Project - Responsive Images by Using JavaScript/JS/script.js
@@ -1,11 +1,28 @@
 const IMAGES = document.querySelectorAll("img");
 console.log("IMAGES: " , IMAGES);
 
+//a function to create srcset attribute for each images
+function makeSrcset (imgScr) {
+  let markup = [];
+  let width = 400;
+
+  for (let i=0; i < 5 ; i++) {
+    markup[i] = imgScr + "-" + width + ".jpg " + width + "w";
+    width += 400;
+  }
+
+  return markup.join();
+}
+
+
 for (let i=0; i < IMAGES.length ; i++) {
-  let imgScr = IMAGES[i].getAttribute("src");
+  let imgSrc = IMAGES[i].getAttribute("src");
   // to cut off all endings which indicate the size of IMAGES
-  imgScr = imgScr.slice(0 , -8);
-  console.log("imgScr: " , imgScr);
+  imgSrc = imgSrc.slice(0 , -8);
+  console.log("imgScr: " , imgSrc);
+
+  let srcset = makeSrcset(imgSrc);
+  console.log("srcset: " , srcset);
 
   //then, the data-type of images extracted
   let type = IMAGES[i].getAttribute("data-type");
